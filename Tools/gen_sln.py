d0180a3ccc33c8563749770bba641d720b683a97
Dmytro <dvshalimov@gmail.com>
2024-05-06 15:39:38 +0300

    Update gen_sln.py

--- Tools/gen_sln.py
+++ Tools/gen_sln.py
@@ -1,10 +1,20 @@
 import subprocess
-import sys
+import argparse
 
 CONFIGURATIONS = "Debug;MinSizeRel;DebugServer;MinSizeRelServer"
 
-subprocess.check_call(["cmake",
-    f"-DCMAKE_CONFIGURATION_TYPES={CONFIGURATIONS}", "-S", ".", "-B", "./bin"])
 
-if "--open" in sys.argv:
-    subprocess.Popen(["start", "bin/SpaceGame.sln"], shell=True)
+def _main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--open", action="store_true", help="Open solution")
+    args = parser.parse_args()
+
+    subprocess.check_call(["cmake",
+        f"-DCMAKE_CONFIGURATION_TYPES={CONFIGURATIONS}", "-S", ".", "-B", "./bin"])
+
+    if args.open:
+        subprocess.Popen(["start", "bin/SpaceGame.sln"], shell=True)
+
+
+if __name__ == "__main__":
+    _main()
