d5593c25649bdcb77b3eefc83f41c47dda48bd09
Dmytro <dvshalimov@gmail.com>
2024-05-07 16:09:05 +0300

    out of bounds screen dark effect

--- Source/Game/Systems/WorldBoundsSystem.cpp
+++ Source/Game/Systems/WorldBoundsSystem.cpp
@@ -1,7 +1,6 @@
 #include "WorldBoundsSystem.h"
 #include "../World.h"
 #include "../SystemsManager.h"
-#include <iostream>
 
 void WorldBoundsSystem::init(World& w, SystemsManager& sm)
 {
@@ -30,16 +29,41 @@ void WorldBoundsSystem::update(World& w)
     w.forEach<PlayerComponent>([this, &w](const Entity ent, PlayerComponent& comp)
         {
             const auto& player_transform{ *w.tryGetComponent<TransformComponent>(ent) };
-            if (!inRange(player_transform.location.x, -m_bounds_pixel_size, m_bounds_pixel_size) ||
-                !inRange(player_transform.location.y, -m_bounds_pixel_size, m_bounds_pixel_size))
+            const std::vector<Entity> out_of_bounds_ents{w.getEntities<OutOfWorldBoundsComponent>()};
+            if (isPlayerInRange(player_transform))
             {
-                const std::vector<Entity> out_of_bounds_ents{w.getEntities<OutOfWorldBoundsComponent>()};
+                if (out_of_bounds_ents.size() != 0)
+                {
+                    w.destroyEntity(*out_of_bounds_ents.begin());
+                }
+            }
+            else
+            {
                 if (out_of_bounds_ents.size() == 0)
                 {
                     createOutOfBoundsEntity(w, player_transform);
+                    m_fade_in_start = SDL_GetTicks64();
+                }
+                else
+                {
+                    const Uint64 current_time_ms{SDL_GetTicks64()};
+                    const Uint64 diff{ current_time_ms - m_fade_in_start };
+                    if (diff < m_fade_in_time_ms)
+                    {
+                        auto& render_comp{*w.tryGetComponent<UIRenderComponent>(*out_of_bounds_ents.begin())};
+                        const float delta{static_cast<float>(diff) / m_fade_in_timef_ms };
+                        constexpr float max_opacity{255.0f};
+                        SDL_SetTextureAlphaMod(render_comp.texture, static_cast<Uint8>(max_opacity * delta));
+                    }
                 }
             }
         });
+}
+
+bool WorldBoundsSystem::isPlayerInRange(const TransformComponent& player_transform) const
+{
+    return inRange(player_transform.location.x, -m_bounds_pixel_size, m_bounds_pixel_size) &&
+        inRange(player_transform.location.y, -m_bounds_pixel_size, m_bounds_pixel_size);
 }
 
 bool WorldBoundsSystem::inRange(const float value, const float min_value, const float max_value) const
@@ -55,9 +79,11 @@ void WorldBoundsSystem::createOutOfBoundsEntity(World& w, const TransformCompone
     auto& render_comp{w.addComponent<UIRenderComponent>(e)};
     render_comp.layer = UIRenderLayer::OUT_OF_WORLD_BOUNDS_EFFECT;
     render_comp.texture = m_render_system->getTexture(TextureType::white_pixel).texture;
+    
     const SDL_Point screen_size{m_render_system->getScreenSize()};
     render_comp.src.x = render_comp.src.y = render_comp.dst.x = render_comp.dst.y = 0;
     render_comp.src.w = render_comp.dst.w = screen_size.x;
     render_comp.src.h = render_comp.dst.h = screen_size.y;
     SDL_SetTextureColorMod(render_comp.texture, 0, 0, 0);
+    SDL_SetTextureAlphaMod(render_comp.texture, 0);
 }
--- Source/Game/Systems/WorldBoundsSystem.h
+++ Source/Game/Systems/WorldBoundsSystem.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "SDL.h"
 
 class World;
 class SystemsManager;
@@ -14,11 +15,17 @@ public:
 	void shutdown() {}
 
 private:
+	bool isPlayerInRange(const TransformComponent& player_transform) const;
 	bool inRange(const float value, const float min_value, const float max_value) const;
 	void createOutOfBoundsEntity(World& w, const TransformComponent& player_transform);
 
 private:
-	const float m_bounds_pixel_size{1000.0f};
-	const int m_grid_size{15};
+	const float m_bounds_pixel_size{3500.0f};
+	const int m_grid_size{20};
+
+	const Uint64 m_fade_in_time_ms{7000};
+	const float m_fade_in_timef_ms{static_cast<float>(m_fade_in_time_ms)};
+	Uint64 m_fade_in_start{};
+
 	RenderSystem* m_render_system{nullptr};
 };
