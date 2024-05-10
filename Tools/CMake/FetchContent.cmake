abf9355d9cca78dea58624a0e60eb73020220ee2
Dmytro <dvshalimov@gmail.com>
2024-05-10 16:42:44 +0300

    update sdl versions

--- Tools/CMake/FetchContent.cmake
+++ Tools/CMake/FetchContent.cmake
@@ -5,14 +5,14 @@ include(FetchContent)
 FetchContent_Declare(
     SDL2
     GIT_REPOSITORY  https://github.com/libsdl-org/SDL
-    GIT_TAG         15ead9a40d09a1eb9972215cceac2bf29c9b77f6
+    GIT_TAG         fb1497566c5a05e2babdcf45ef0ab5c7cca2c4ae
     GIT_PROGRESS    TRUE
 )
 
 FetchContent_Declare(
     SDL2Image
     GIT_REPOSITORY  https://github.com/libsdl-org/SDL_image
-    GIT_TAG         859357d2e5e82e002f54f440cc031e174e58e749
+    GIT_TAG         abcf63aa71b4e3ac32120fa9870a6500ddcdcc89
     GIT_PROGRESS    TRUE
 )
 
